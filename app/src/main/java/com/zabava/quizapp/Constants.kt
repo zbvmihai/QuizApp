48b73384007f16eda192e2992f16da61d16c888a
Zabi <zbvmihai@gmail.com>
2023-03-06 11:51:05 +0000

    add quiz question activity and questions

---
+++ app/src/main/java/com/zabava/quizapp/Constants.kt
@@ -0,0 +1,98 @@
+package com.zabava.quizapp
+
+object Constants {
+
+    fun getQuestions(): ArrayList<Question>{
+        val questionsList = ArrayList<Question>()
+
+        val q1 = Question(
+            1, "What country does this flag belong to?",
+            R.drawable.ic_flag_of_argentina,
+            "Argentina","Australia","Romania","Germany",
+            1
+        )
+        questionsList.add(q1)
+        val q2 = Question(
+            2, "What country does this flag belong to?",
+            R.drawable.ic_flag_of_australia,
+            "Angola", "Austria",
+            "Australia", "Armenia", 3
+        )
+
+        questionsList.add(q2)
+
+        val q3 = Question(
+            3, "What country does this flag belong to?",
+            R.drawable.ic_flag_of_brazil,
+            "Belarus", "Belize",
+            "Brunei", "Brazil", 4
+        )
+
+        questionsList.add(q3)
+
+        val q4 = Question(
+            4, "What country does this flag belong to?",
+            R.drawable.ic_flag_of_belgium,
+            "Bahamas", "Belgium",
+            "Barbados", "Belize", 2
+        )
+
+        questionsList.add(q4)
+
+        val q5 = Question(
+            5, "What country does this flag belong to?",
+            R.drawable.ic_flag_of_fiji,
+            "Gabon", "France",
+            "Fiji", "Finland", 3
+        )
+
+        questionsList.add(q5)
+
+        val q6 = Question(
+            6, "What country does this flag belong to?",
+            R.drawable.ic_flag_of_germany,
+            "Germany", "Georgia",
+            "Greece", "none of these", 1
+        )
+
+        questionsList.add(q6)
+
+        val q7 = Question(
+            7, "What country does this flag belong to?",
+            R.drawable.ic_flag_of_denmark,
+            "Dominica", "Egypt",
+            "Denmark", "Ethiopia", 3
+        )
+
+        questionsList.add(q7)
+
+        val q8 = Question(
+            8, "What country does this flag belong to?",
+            R.drawable.ic_flag_of_india,
+            "Ireland", "Iran",
+            "Hungary", "India", 4
+        )
+
+        questionsList.add(q8)
+
+        val q9 = Question(
+            9, "What country does this flag belong to?",
+            R.drawable.ic_flag_of_new_zealand,
+            "Australia", "New Zealand",
+            "Tuvalu", "United States of America", 2
+        )
+
+        questionsList.add(q9)
+        val q10 = Question(
+            10, "What country does this flag belong to?",
+            R.drawable.ic_flag_of_kuwait,
+            "Kuwait", "Jordan",
+            "Sudan", "Palestine", 1
+        )
+
+        questionsList.add(q10)
+
+
+        return questionsList
+    }
+}
---
+++ app/src/main/java/com/zabava/quizapp/QuizQuestionsActivity.kt
@@ -0,0 +1,15 @@
+package com.zabava.quizapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class QuizQuestionsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quiz_questions)
+
+        val questionsList = Constants.getQuestions()
+        Log.i("QuestionsList size is", "${questionsList.size}")
+    }
+}
